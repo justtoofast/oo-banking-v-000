1896b38939a896159d94a7c3ab38f0473609304b
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-08 20:17:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.valid? == false
+    if valid? == false
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     else
